20811e93c9d6d37a79a496fb08c5069586427272
Dwayne Campogan <dcampogan@gmail.com>
2020-01-07 18:40:05 -0800

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,22 +5,33 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
 	constructor(props) {
     super(props);
     this.state = {
-        campsites: CAMPSITES
+        campsites: CAMPSITES,
+        comments: COMMENTS,
+        partners: PARTNERS,
+        promotions: PROMOTIONS
     };
 }
 
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home 
+          campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+          promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+          partner={this.state.partners.filter(partner => partner.featured)[0]}
+        />
       );
     }
 
@@ -29,8 +40,10 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
+          {/* if need to pass state data use render syntax like below otherwise, use component attribute like above*/}
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-          <Redirect to='/home' /> {/* will redirect here if no route is found */}
+          <Route exact path='/contactus' component={Contact} />
+          <Redirect to='/home' />
         </Switch>
         <Footer />
       </div>
