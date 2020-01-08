b89a097158fa1edcd2fec404fae63758ccbb268d
Dwayne Campogan <dcampogan@gmail.com>
2020-01-07 20:36:56 -0800

    Added Breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,71 +1,149 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Card,
   CardImg,
   CardText,
   CardBody,
-  CardTitle
+  Breadcrumb,
+  BreadcrumbItem
 } from "reactstrap";
+import { Link } from "react-router-dom";
 
-class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function RenderCampsite({campsite}) {
+  return (
+    <div className="col-md-5 m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
-  renderCampsite(campsite) {
+function RenderComments({comments}) {
+      if (comments) {
+        return (
+          <div className="col-md-5 m-1">
+            <h4>Comments</h4>
+            {comments.map(comment => (
+              <div key={comment.id}>
+                <div>{comment.text}</div>
+                <div className="mb-3">
+                  -- {comment.author},{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit"
+                  }).format(new Date(Date.parse(comment.date)))}
+                </div>
+              </div>
+            ))}
+          </div>
+        );
+      }
+      return <div></div>;
+    }
+
+function CampsiteInfo(props) {
+  if (props.campsite) {
     return (
-      <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>{props.campsite.name}</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.comments} />
+        </div>
       </div>
     );
   }
-
-  renderComments(comments) {
-    if (comments) {
-      return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map(comment => (
-            <div key={comment.id}>
-              <div>{comment.text}</div>
-              <div className="mb-3">
-                -- {comment.author},{" "}
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "2-digit"
-                }).format(new Date(Date.parse(comment.date)))}
-              </div>
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
-  render() {
-    if (this.props.campsite) {
-      return (
-        <div className="container">
-          <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-          </div>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
+  return <div></div>;
 }
 
 export default CampsiteInfo;
+
+
+
+//-------------------------------------------------
+
+// class CampsiteInfo extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+
+//   renderCampsite(campsite) {
+//     return (
+//       <div className="col-md-5 m-1">
+//         <Card>
+//           <CardImg top src={campsite.image} alt={campsite.name} />
+//           <CardBody>
+//             <CardText>{campsite.description}</CardText>
+//           </CardBody>
+//         </Card>
+//       </div>
+//     );
+//   }
+
+//   renderComments(comments) {
+//     if (comments) {
+//       return (
+//         <div className="col-md-5 m-1">
+//           <h4>Comments</h4>
+//           {comments.map(comment => (
+//             <div key={comment.id}>
+//               <div>{comment.text}</div>
+//               <div className="mb-3">
+//                 -- {comment.author},{" "}
+//                 {new Intl.DateTimeFormat("en-US", {
+//                   year: "numeric",
+//                   month: "short",
+//                   day: "2-digit"
+//                 }).format(new Date(Date.parse(comment.date)))}
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       );
+//     }
+//     return <div></div>;
+//   }
+
+//   //THIS IS DIFFERENT FROM MINAE'S CODE IN THE VIDEO - SHE HAS A FUNCTION COMPONENT (DID I MISS SOMETHING?!)
+//   render() {
+//     if (this.props.campsite) {
+//       return (
+//         <div className="container">
+//           <div className="row">
+//             <div className="col">
+//               <Breadcrumb>
+//                 <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+//                 <BreadcrumbItem active>{this.props.campsite.name}</BreadcrumbItem>
+//               </Breadcrumb>
+//               <h2>{this.props.campsite.name}</h2>
+//               <hr />
+//             </div>
+//           </div>
+//           <div className="row">
+//             {this.renderCampsite(this.props.campsite)}
+//             {this.renderComments(this.props.comments)}
+//           </div>
+//         </div>
+//       );
+//     }
+//     return <div></div>;
+//   }
+// }
+
+// export default CampsiteInfo;
